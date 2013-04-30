79b3885f155e3317fc1d96bbb9a4de08ce3ce87a
Jing Yang <ditsing@gmail.com>
2013-04-30 18:19:44 +0800

    Initial commit of driver project.

---
+++ driver/xfsd_driver.cpp
@@ -0,0 +1,71 @@
+#include "stdafx.h"
+
+void xfsd_driverUnload(IN PDRIVER_OBJECT DriverObject);
+NTSTATUS xfsd_driverCreateClose(IN PDEVICE_OBJECT DeviceObject, IN PIRP Irp);
+NTSTATUS xfsd_driverDefaultHandler(IN PDEVICE_OBJECT DeviceObject, IN PIRP Irp);
+
+#ifdef __cplusplus
+extern "C" NTSTATUS DriverEntry(IN PDRIVER_OBJECT DriverObject, IN PUNICODE_STRING  RegistryPath);
+#endif
+
+NTSTATUS DriverEntry(IN PDRIVER_OBJECT DriverObject, IN PUNICODE_STRING  RegistryPath)
+{
+	UNICODE_STRING DeviceName,Win32Device;
+	PDEVICE_OBJECT DeviceObject = NULL;
+	NTSTATUS status;
+	unsigned i;
+
+	RtlInitUnicodeString(&DeviceName,L"\\Device\\xfsd_driver0");
+	RtlInitUnicodeString(&Win32Device,L"\\DosDevices\\xfsd_driver0");
+
+	for (i = 0; i <= IRP_MJ_MAXIMUM_FUNCTION; i++)
+		DriverObject->MajorFunction[i] = xfsd_driverDefaultHandler;
+
+	DriverObject->MajorFunction[IRP_MJ_CREATE] = xfsd_driverCreateClose;
+	DriverObject->MajorFunction[IRP_MJ_CLOSE] = xfsd_driverCreateClose;
+	
+	DriverObject->DriverUnload = xfsd_driverUnload;
+	status = IoCreateDevice(DriverObject,
+							0,
+							&DeviceName,
+							FILE_DEVICE_UNKNOWN,
+							0,
+							FALSE,
+							&DeviceObject);
+	if (!NT_SUCCESS(status))
+		return status;
+	if (!DeviceObject)
+		return STATUS_UNEXPECTED_IO_ERROR;
+
+	DeviceObject->Flags |= DO_DIRECT_IO;
+	DeviceObject->AlignmentRequirement = FILE_WORD_ALIGNMENT;
+	status = IoCreateSymbolicLink(&Win32Device, &DeviceName);
+
+	DeviceObject->Flags &= ~DO_DEVICE_INITIALIZING;
+
+	return STATUS_SUCCESS;
+}
+
+void xfsd_driverUnload(IN PDRIVER_OBJECT DriverObject)
+{
+	UNICODE_STRING Win32Device;
+	RtlInitUnicodeString(&Win32Device,L"\\DosDevices\\xfsd_driver0");
+	IoDeleteSymbolicLink(&Win32Device);
+	IoDeleteDevice(DriverObject->DeviceObject);
+}
+
+NTSTATUS xfsd_driverCreateClose(IN PDEVICE_OBJECT DeviceObject, IN PIRP Irp)
+{
+	Irp->IoStatus.Status = STATUS_SUCCESS;
+	Irp->IoStatus.Information = 0;
+	IoCompleteRequest(Irp, IO_NO_INCREMENT);
+	return STATUS_SUCCESS;
+}
+
+NTSTATUS xfsd_driverDefaultHandler(IN PDEVICE_OBJECT DeviceObject, IN PIRP Irp)
+{
+	Irp->IoStatus.Status = STATUS_NOT_SUPPORTED;
+	Irp->IoStatus.Information = 0;
+	IoCompleteRequest(Irp, IO_NO_INCREMENT);
+	return Irp->IoStatus.Status;
+}
