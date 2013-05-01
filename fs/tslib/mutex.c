505f00b71d41a094cd65ea88428572c7bd32e81b
Jing Yang <ditsing@gmail.com>
2013-05-01 20:29:54 +0800

    Lock for mutex init.

--- fs/tslib/mutex.c
+++ fs/tslib/mutex.c
@@ -11,11 +11,16 @@ struct kernel_mutex
 };
 
 struct kernel_mutex inline_kmutex;
+static pthread_mutex_t init_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void mutex_init( struct mutex *m)
 {
+	pthread_mutex_lock( &init_mutex);
+
 	m->kmutex = ( struct kernel_mutex *) mem_alloc( sizeof( pthread_mutex_t));
 	pthread_mutex_init( ( pthread_mutex_t *)&m->kmutex, NULL);
+
+	pthread_mutex_unlock( &init_mutex);
 }
 
 void mutex_lock( struct mutex *m)
