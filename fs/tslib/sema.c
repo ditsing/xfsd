41db46cb5094ec096abddb7e9f44c9212244e59a
Jing Yang <ditsing@gmail.com>
2013-05-02 16:36:33 +0800

    Semaphore under Win32.

--- fs/tslib/sema.c
+++ fs/tslib/sema.c
@@ -5,30 +5,29 @@
 
 struct kerenl_sem
 {
-	sem_t _;
+	KSEMAPHORE _;
 };
 
 int sema_init( struct semaphore *sem, int value)
 {
-	sem->sem = ( struct kerenl_sem*)mem_alloc( sizeof( sem_t));
-	return sem_init( ( sem_t*)sem->sem, 0, value);
+	sem->sem = ( struct kerenl_sem*)ddk_mem_alloc( sizeof( KSEMAPHORE), 0);
+	return KeInitializeSemaphore( ( KSEMAPHORE*)sem->sem, 0, value), 0;
 }
 
 int down( struct semaphore *sem)
 {
-	return sem_wait( ( sem_t*)sem->sem);
+	return KeWaitForSingleObject( ( KSEMAPHORE*)sem->sem, Executive, KernelMode, FALSE, NULL);
 }
 
 int up( struct semaphore *sem)
 {
-	return sem_post( ( sem_t*)sem->sem);
+	return KeReleaseSemaphore( ( KSEMAPHORE*)sem->sem, IO_NO_INCREMENT,1,FALSE);
 }
 
 int down_trylock( struct semaphore *sem)
 {
-	return sem_trywait( ( sem_t*)sem->sem);
+	return KeWaitForSingleObject( ( KSEMAPHORE*)sem->sem, Executive, KernelMode, FALSE, 0) != STATUS_SUCCESS;
 }
-
 
 #else
 #include <pthread.h>
