cd2ff1ecf0e4e2e98a3c58b1523dbf0b3823d972
Jing Yang <ditsing@gmail.com>
2013-05-05 22:10:05 +0800

    Typos.

--- fs/build/build_win.bat
+++ fs/build/build_win.bat
@@ -6,5 +6,5 @@ move xfs\xfs_inode_item.h.win xfs\xfs_inode_item.h
 move xfs\xfs_dinode.h xfs\xfs_dinode.h.linux
 move xfs\xfs_dinode.h.win xfs\xfs_dinode.h
 
-move xfs\xfs_dir2_formate.h xfs\xfs_dir2_formate.h.linux
-move xfs\xfs_dir2_formate.h.win xfs\xfs_dir2_formate.h
+move xfs\xfs_dir2_format.h xfs\xfs_dir2_format.h.linux
+move xfs\xfs_dir2_format.h.win xfs\xfs_dir2_format.h
